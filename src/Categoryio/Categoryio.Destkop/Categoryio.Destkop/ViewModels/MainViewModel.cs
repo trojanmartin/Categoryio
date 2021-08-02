057b27d9ff3a8c2b9225b8c367bb295cf6d80efe
Martin <martin.trojan.372@gmail.com>
2021-08-02 23:03:35 +0200

    feat: add datagrid

--- src/Categoryio/Categoryio.Destkop/Categoryio.Destkop/ViewModels/MainViewModel.cs
+++ src/Categoryio/Categoryio.Destkop/Categoryio.Destkop/ViewModels/MainViewModel.cs
@@ -18,7 +18,31 @@ namespace Categoryio.Destkop.ViewModels
                 new Category()
                 {
                     Name = "Animals",
-                    Created = System.DateTime.Now
+                    Created = System.DateTime.Now,
+                    Items = new List<Item>()
+                    {
+                        new Item()
+                        {
+                            Id = 1,
+                            Name = "Stolna hra",
+                            Description = "pre 5 deti, super lahke",
+                            Created = System.DateTime.Now
+                        },
+                        new Item()
+                        {
+                            Id = 2,
+                            Name = "fotbal hra",
+                            Description = "nebezpecne",
+                            Created = System.DateTime.Now.AddDays(-10)
+                        },
+                        new Item()
+                        {
+                            Id = 3,
+                            Name = "Pingpong hra",
+                            Description = "pre 5 deti, super tazke",
+                            Created = System.DateTime.Now.AddDays(5)
+                        },
+                    }
                 },
                 new Category()
                 {
